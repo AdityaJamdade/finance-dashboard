def44fec937708e57383c9d498fb81d406dcba04
AdityaJamdade <adijamdade156@gmail.com>
2023-11-25 10:02:16 +0530

    added recharts library

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,15 +1,20 @@
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 type Props = {};
 
 const Row1 = (props: Props) => {
   const { data } = useGetKpisQuery();
-  console.log('Data:' , data);
+  console.log("Data:", data);
 
   return (
     <>
-      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="a">
+
+        
+
+      </DashboardBox>
       <DashboardBox gridArea="b"></DashboardBox>
       <DashboardBox gridArea="c"></DashboardBox>
     </>
