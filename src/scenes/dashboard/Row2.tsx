7d2374ca94cd27e61db5da91a1123387caa8b2ac
AdityaJamdade <adijamdade156@gmail.com>
2023-12-04 13:08:44 +0530

    added pie chart supplement text in row 2

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -1,21 +1,17 @@
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { useMemo } from "react";
 import {
-  AreaChart,
-  Area,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
   Line,
-  Legend,
   LineChart,
-  BarChart,
-  Bar,
   PieChart,
   Pie,
   Cell,
@@ -101,29 +97,52 @@ const Row1 = () => {
         </ResponsiveContainer>
       </DashboardBox>
       <DashboardBox gridArea="e">
-        <PieChart
-          width={110}
-          height={100}
-          margin={{
-            top: 0,
-            right: -10,
-            left: 10,
-            bottom: 0,
-          }}
-        >
-          <Pie
-            stroke="none"
-            data={pieData}
-            innerRadius={18}
-            outerRadius={38}
-            paddingAngle={2}
-            dataKey="value"
+        <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
           >
-            {pieData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={pieColors[index]} />
-            ))}
-          </Pie>
-        </PieChart>
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+              Finance Goals of the campaign that is desired
+            </Typography>
+          </Box>
+
+          <Box flexBasis="40%">
+            <Typography variant="h5">Losses in revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Profit Margins
+            </Typography>
+            <Typography variant="h6">
+              Margins are up by 30% from last month
+            </Typography>
+          </Box>
+        </FlexBetween>
       </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
