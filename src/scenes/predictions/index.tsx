a74fab5b851dc53f4799a116415930a69dea47a2
AdityaJamdade <adijamdade156@gmail.com>
2023-12-13 09:14:00 +0530

    added base layout for predictions page

--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -1,10 +1,23 @@
+import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
+import { useState } from "react";
 
 type Props = {};
 
 const Predictions = (props: Props) => {
   const { palette } = useTheme();
-  return <div>Predictions</div>;
+  const [isPredictions, setIsPredictions] = useState(false);
+  const { data: kpiData } = useGetKpisQuery();
+
+  return (
+    <DashboardBox
+      width="100%"
+      height="100%"
+      p="1rem"
+      overflow="hidden"
+    ></DashboardBox>
+  );
 };
 
 export default Predictions;
