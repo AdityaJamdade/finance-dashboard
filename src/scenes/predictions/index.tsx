0d593a776f53f29b571b8aa0fa38ca09d6e151bb
AdityaJamdade <adijamdade156@gmail.com>
2023-12-14 21:57:09 +0530

    error at client predictions page

--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -1,6 +1,7 @@
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 import { useGetKpisQuery } from "@/state/api";
-import { useTheme } from "@mui/material";
+import { useTheme, Box, Typography, Button } from "@mui/material";
 import { useState } from "react";
 
 type Props = {};
@@ -11,12 +12,19 @@ const Predictions = (props: Props) => {
   const { data: kpiData } = useGetKpisQuery();
 
   return (
-    <DashboardBox
-      width="100%"
-      height="100%"
-      p="1rem"
-      overflow="hidden"
-    ></DashboardBox>
+    <DashboardBox width="100%" height="1rem" p="1rem" overflow="hidden">
+      <FlexBetween m="1rem 2.5rem">
+        <Box>
+          <Typography variant="h3">Revenue and Predictions</Typography>
+          <Typography variant="h6">
+            charted revenue and predicted revenue based on a simple linear
+            regression model
+          </Typography>
+        </Box>
+
+        <Button onClick={() => setIsPredictions(!isPredictions)}></Button>
+      </FlexBetween>
+    </DashboardBox>
   );
 };
 
